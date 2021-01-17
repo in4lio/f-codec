4bd69dfb9b686105d85405acfc4b9af604ee1a49
Vitaly Kravtsov <in4lio@gmail.com>
2021-01-17 17:47:21 +0100

    0.1.1

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+rm -r build
+rm -r f_codec.egg-info
+
 python3 -m pip uninstall -y f-codec
-python3 setup.py sdist bdist_wheel
-python3 -m pip install --user f-codec -f dist
+python3 setup.py sdist
+python3 -m pip install -v f-codec -f dist
